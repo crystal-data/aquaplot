fa366d5a3c1806c6c763f745a4759db3d5e30fee
Chris Zimmerman <chris-zimmerman@live.com>
2019-10-01 15:59:10 -0500

    refactored core to include new templates

---
+++ src/aquaplot/core/figures/function.cr
@@ -0,0 +1,24 @@
+require "../meta/line_options"
+
+module FunctionModule
+  include LineOptionsModule
+  class Function < LineOptions
+    property function : String
+    property style : String = "lines"
+
+    def set_style(@style)
+    end
+
+    def set_function(@function)
+    end
+
+    def initialize(@function, @style="lines", **options)
+      super(**options)
+    end
+
+    def to_s
+      parent = super
+      return "#{function} with #{style} #{parent}"
+    end
+  end
+end
---
+++ src/aquaplot/core/plot/plot2d.cr
@@ -0,0 +1,53 @@
+require "../meta/gnuplot_options"
+require "../meta/line_options"
+require "../figures/function"
+require "uuid"
+
+module PlotModule
+  include GnuOptionsModule
+  include LineOptionsModule
+  include FunctionModule
+  class Plot(T) < GnuOptions
+    property figures : Array(T)
+
+    def initialize(@figures : Array(T), **options)
+      super(**options)
+      process_style(@figures)
+    end
+
+    def process_style(figures : Array(LineOptions))
+      @figures.each_with_index do |el, index|
+        el.set_linestyle(index+1)
+      end
+    end
+
+    def initialize(figure : T, **options)
+      super(**options)
+      @figures = [figure]
+      process_style(@figures)
+    end
+
+    def to_s
+      parent = super
+      figs = @figures.map do |fig|
+        fig.to_s
+      end.join(", ")
+      return "#{parent}\nplot #{figs}"
+    end
+
+    def show
+      fname = "/tmp/#{UUID.random.to_s}"
+      File.write(fname, self.to_s)
+      gnuplot_dispatch(fname)
+      File.delete(fname)
+    end
+
+    private def gnuplot_dispatch(fname)
+      stdout = IO::Memory.new
+      Process.run("gnuplot -p #{fname}", shell: true, output: stdout)
+      if stdout
+        puts stdout
+      end
+    end
+  end
+end
