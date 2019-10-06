e360abde76d300bf64c3f2e61df79afc79cf2696
Chris Zimmerman <chris-zimmerman@live.com>
2019-10-05 20:06:59 -0500

    [ENH] Bar and scatter plot functionality #10

--- src/aquaplot/core/core.cr
+++ src/aquaplot/core/core.cr
@@ -2,10 +2,14 @@ require "./plot/plot2d"
 require "./plot/plot3d"
 require "./series/function"
 require "./series/line"
+require "./series/scatter"
+require "./series/bar"
 
 module CoreModule
   include Plot2DModule
   include Plot3DModule
   include FunctionModule
   include LineModule
+  include ScatterModule
+  include BarModule
 end
---
+++ src/aquaplot/core/series/bar.cr
@@ -0,0 +1,55 @@
+require "./base"
+require "../common/helpers"
+require "../common/exceptions"
+
+module BarModule
+  include SeriesBaseModule
+
+  class Bar < XorXY
+    #
+    # INITIALIZATION
+    #
+    property style : String = "boxes"
+    property fillstyle : String
+
+    def initialize(
+      x : Indexable(Number),
+      @fillstyle = "solid 0.25",
+      **options
+    )
+      super(x, **options, linewidth: 1)
+    end
+
+    def initialize(
+      x : Indexable(Number),
+      y : Indexable(Number),
+      @fillstyle = "solid 0.25",
+      **options
+    )
+      super(x, y, **options, linewidth: 1)
+    end
+
+    #
+    # GETTERS
+    #
+    def get_fillstyle
+      _option_to_string "fs", @fillstyle
+    end
+
+    #
+    # SETTERS
+    #
+    def set_fillstyle(@fillstyle)
+    end
+
+    #
+    # SERIALIZER
+    #
+    def to_s
+      [
+        super,
+        get_fillstyle,
+      ].join(" ")
+    end
+  end
+end
