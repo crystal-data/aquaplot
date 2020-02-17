231103ca21121807274713cde7355cefc6eeb9fb
Chris Zimmerman <chris-zimmerman@live.com>
2020-02-17 11:03:01 -0600

    Update plot3d.cr

--- src/plot/plot3d.cr
+++ src/plot/plot3d.cr
@@ -1,11 +1,32 @@
 require "./base"
 
+struct AquaPlot::View
+  getter rotation_x : Int32? = nil
+  getter rotation_z : Int32? = nil
+  getter scale : Float64 = 1.0
+
+  def to_s
+    "set view #{@rotation_x},#{@rotation_z},#{@scale}"
+  end
+
+  def initialize(@rotation_x : Int32? = nil, @rotation_z : Int32? = nil, @scale : Float64 = 1.0)
+  end
+end
+
 class AquaPlot::Plot3D(T) < AquaPlot::PlotBase(T)
+  property view : View = View.new
+
+  def set_view(rx : Int32, ry : Int32, scale : Float64 = 1.0)
+    @view = View.new(rx, ry, scale)
+  end
+
   def to_s
     figs = @figures.map do |fig|
       fig.to_s
     end.join(", ")
 
-    "#{super}\nsplot#{figs}"
+    s = "#{@view.to_s}\n"
+
+    s + "#{super}\nsplot#{figs}"
   end
 end
